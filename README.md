5d7614bfbda6f1c18fde84ea873c7151b81680e1
真芙辣 <a152302202531@outlook.com>
2025-02-17 23:20:26 +0800

    改用rikka hidden-api README添加贡献信息 使用ServerTemplate

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-## ShamikoX
+# ShamikoX
 
 可以应用内或快速设置页面快速开关Shamiko白名单模式，并可以为前台app申请root权限
 
 不多次开启su会话，使用一个常驻server进行root操作，减少部分开销
 
-## 下载
+# 下载
 
 [Github](https://github.com/yangFenTuoZi/ShamikoX/releases)
 
-## tips
-
-server.cpp socket部分代码是AI的
+# 提示
 
 应用在后台时，server与daemon可能会被Magisk杀死，推荐使用[Runner](https://github.com/yangFenTuoZi/Runner)
 或[ShizukuRunner](https://github.com/WuDi-ZhanShen/ShizukuRunner)或Magisk模块启动server
 
 部分国产UI/OS可能需要设置 电池使用无限制、电池优化白名单 才能正常进行socket通信
+
+# 贡献
+
+- [PackageViewer](https://github.com/MaYiFei1995/PackageViewer): 提供选择app功能，用于自定义app授予root权限
+- [ServerTemplate](https://github.com/yangFenTuoZi/SeverTemplate): 服务模板，更简单实现服务功能
+- [HiddenApi](https://github.com/RikkaW/HiddenApi): 调用隐藏api
--- app/src/main/java/yangFenTuoZi/shamikox/server/Server.java
+++ app/src/main/java/yangFenTuoZi/shamikox/server/Server.java
@@ -1,123 +1,97 @@
 package yangFenTuoZi.shamikox.server;
 
-import static yangFenTuoZi.shamikox.server.Logger.getStackTraceString;
+import static yangFenTuoZi.server.Logger.getStackTraceString;
 import static yangFenTuoZi.shamikox.server.magisk.SuPolicy.POLICY_ALLOW;
 
 import android.annotation.SuppressLint;
 import android.app.ActivityManager;
-import android.app.AlertDialog;
 import android.app.IActivityManager;
 import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
-import android.content.pm.IPackageManager;
 import android.content.pm.PackageManager;
-import android.ddm.DdmHandleAppName;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.IBinder;
-import android.os.Looper;
 import android.os.RemoteException;
 import android.os.ServiceManager;
-import android.system.Os;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.StyleSpan;
 import android.widget.Toast;
 
-import androidx.core.content.ContextCompat;
-
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-
 import java.io.File;
 import java.lang.reflect.Method;
 import java.util.List;
 
+import yangFenTuoZi.server.Logger;
+import yangFenTuoZi.server.ServerTemplate;
+import yangFenTuoZi.server.fakecontext.FakeContext;
 import yangFenTuoZi.shamikox.BuildConfig;
-import yangFenTuoZi.shamikox.MainActivity;
-import yangFenTuoZi.shamikox.server.fakecontext.FakeContext;
-import yangFenTuoZi.shamikox.server.fakecontext.Workarounds;
 import yangFenTuoZi.shamikox.server.magisk.MagiskDB;
 import yangFenTuoZi.shamikox.server.magisk.SuPolicy;
 
-public class Server {
+public class Server extends ServerTemplate {
     public static final String TAG = "shamikox_server";
     public static final String ACTION_SERVER_RUNNING = "shamikox.intent.action.SERVER_RUNNING";
     public static final String ACTION_SERVER_STOPPED = "shamikox.intent.action.SERVER_STOPPED";
     public static final String SEND_BINDER_FILE = "send_binder";
-    Handler mHandler;
     MagiskDB magiskDB;
     PackageManager packageManager;
-    IPackageManager iPackageManager;
-    IActivityManager iActivityManager;
     Logger Log;
     boolean isStop = false;
     String appPath;
-    ContextWrapper mContext;
+    FakeContext mContext;
     Context appContext;
     File whiteListFile = new File("/data/adb/shamiko/whitelist");
     IBinder binder;
 
     public static void main(String[] args) {
-        DdmHandleAppName.setAppName(TAG, 0);
-        int uid = Os.getuid();
-        if (uid != 0) {
-            System.err.printf("Insufficient permission! Need to be launched by root, but your uid is %d.\n", uid);
-            System.exit(255);
+        Args.Builder builder = new Args.Builder();
+        builder.uids = new int[]{0};
+        builder.serverName = TAG;
+        builder.enableFakeContext = true;
+        builder.enableLogger = true;
+        builder.logDir = new File("/data/adb/shamikox_logs");
+        new Server(builder.build());
+    }
+
+    private Server(Args args) {
+        super(args);
+    }
+
+    @Override
+    public void onStart() {
+        Log = getLogger();
+        Log.i("Server Start.");
+
+        File magisk_data = new File(MagiskDB.DB_FILE);
+        if (!magisk_data.exists() || magisk_data.isFile()) {
+            Log.e("Only Magisk is supported.");
+            exit(255);
         }
 
         File shamiko = new File("/data/adb/modules/zygisk_shamiko");
         if (!shamiko.exists() || shamiko.isFile()) {
-            System.err.print("Do not install 'Shamiko' module.\n");
-            System.exit(255);
+            Log.e("Do not install 'Shamiko' module.");
+            exit(255);
         }
 
         File shamikoEnable = new File("/data/adb/shamiko");
         if (!shamikoEnable.exists() || shamikoEnable.isFile()) {
-            System.err.printf("'%s' not found, try to reboot your device.\n", shamikoEnable.getPath());
-            System.exit(255);
+            Log.e("'%s' not found, try rebooting your device.", shamikoEnable.getPath());
+            exit(255);
         }
 
-        new Server(args);
-    }
-
-    @SuppressLint("WrongConstant")
-    private Server(String[] args) {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            if (!isStop)
-                onStop();
-        }));
-
-        mContext = new FakeContext();
+        mContext = getContext();
         try {
             appContext = mContext.createPackageContext(BuildConfig.APPLICATION_ID, Context.CONTEXT_IGNORE_SECURITY);
         } catch (PackageManager.NameNotFoundException e) {
             throw new RuntimeException(e);
         }
 
-        try {
-            onStart(args);
-        } catch (Throwable e) {
-            Log.e(getStackTraceString(e));
-        }
-    }
-
-    public void onStart(String[] args) {
-
-        Log = new Logger(TAG, new File(appContext.getCacheDir(), "logs"));
-
-        if (args.length != 0 && args[0].equals("restart")) {
-            Log.i("Server Restart.");
-        } else {
-            Log.i("Server Start.");
-        }
-
         packageManager = mContext.getPackageManager();
-        iPackageManager = IPackageManager.Stub.asInterface(ServiceManager.getService("package"));
-        iActivityManager = IActivityManager.Stub.asInterface(ServiceManager.getService("activity"));
 
         try {
             appPath = getApplicationInfo(BuildConfig.APPLICATION_ID).sourceDir;
@@ -180,7 +154,7 @@ public class Server {
                     if (file.exists()) {
                         file.delete();
                         if (isAppRunning(BuildConfig.APPLICATION_ID))
-                            mHandler.post(this::sendBinder);
+                            runOnMainThread(this::sendBinder);
                     }
                     try {
                         lock.wait(1000);
@@ -190,12 +164,6 @@ public class Server {
                 }
             }
         }).start();
-
-        if (Looper.getMainLooper() == null) {
-            Looper.prepareMainLooper();
-        }
-        mHandler = new Handler();
-        Looper.loop();
     }
 
     private void collapsePanels() {
@@ -211,27 +179,25 @@ public class Server {
 
     private ApplicationInfo getApplicationInfo(String packageName) throws RemoteException {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            return iPackageManager.getApplicationInfo(packageName, 0L, 0);
+            return mPackageManager.getApplicationInfo(packageName, 0L, 0);
         } else {
-            return iPackageManager.getApplicationInfo(packageName, 0, 0);
+            return mPackageManager.getApplicationInfo(packageName, 0, 0);
         }
     }
 
+    @Override
     public void onStop() {
         isStop = true;
 
-        magiskDB.close();
-        mContext.sendBroadcast(new Intent(Server.ACTION_SERVER_STOPPED)
+        if (magiskDB != null) magiskDB.close();
+        if (mContext != null) mContext.sendBroadcast(new Intent(Server.ACTION_SERVER_STOPPED)
                 .setPackage(BuildConfig.APPLICATION_ID)
                 .addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES));
 
-        Log.i("Server Stop.\n");
-        Log.close();
-    }
-
-    public void exit(int status) {
-        onStop();
-        System.exit(status);
+        if (Log != null) {
+            Log.i("Server Stop.\n");
+            Log.close();
+        }
     }
 
     private void sendBinder() {
@@ -248,9 +214,9 @@ public class Server {
                     .addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
                     .putExtra("data", data);
 
-            if (iActivityManager == null || !iActivityManager.asBinder().pingBinder())
-                iActivityManager = IActivityManager.Stub.asInterface(ServiceManager.getService("activity"));
-            iActivityManager.broadcastIntent(null, intent, null, null, 0, null, null,
+            if (mActivityManager == null || !mActivityManager.asBinder().pingBinder())
+                mActivityManager = IActivityManager.Stub.asInterface(ServiceManager.getService("activity"));
+            mActivityManager.broadcastIntent(null, intent, null, null, 0, null, null,
                     null, -1, null, true, false, 0);
         } catch (Throwable e) {
             Log.e("sendBinder error: %s", getStackTraceString(e));
@@ -367,7 +333,7 @@ public class Server {
                     SpannableString msg = new SpannableString(msgText);
                     msg.setSpan(new StyleSpan(Typeface.BOLD), 0, appName.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
-                    mHandler.post(() -> Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show());
+                   runOnMainThread(() -> Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show());
                 } catch (PackageManager.NameNotFoundException e) {
                     Log.e(getStackTraceString(e));
                 }
